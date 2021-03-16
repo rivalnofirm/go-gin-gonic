e96094e372315ac567a49f1464b8655d07e8b9e4
rivalnofirm <rivalnof2@gmail.com>
2021-03-16 23:05:33 +0700

    Date Created

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/rivalnofirm/bookstore_users-api/utils/date_utils"
 	"github.com/rivalnofirm/bookstore_users-api/utils/errors"
 )
 
@@ -31,6 +32,7 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
 	userDB[user.Id] = user
 	return nil
 }
